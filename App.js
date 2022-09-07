9e896f46178b4ab88aa70d448a823ec44c699046
Lucas Lins <lucas_flins@hotmail.com>
2022-09-06 22:37:36 -0300

    teste

--- App.js
+++ App.js
@@ -1,19 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import axios from 'axios';
+// import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
+// const api = axios.create({
+//   baseURL:'https://weather.contrateumdev.com.br/api/'
+// })
 export default function App() {
+  const [obj,setObj] = useState({
+    city : '',
+    state : ''
+  })
+
+  const [weather,setWeather] = useState()
+
+  function onChangeText(input,text){
+    setObj((entradaArrow)=>{
+      return {
+        ...entradaArrow,
+        [input]:text
+      }
+    })
+    console.log(obj)
+  }
+
+
+  async function getWeather(){
+    // const response = await api.get(`weather/city/?city=${obj.city},${obj.state}`)
+    // console.log(response.data)
+    // setWeather(response.data)
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={{color:'white'}}>Weather!!</Text>
+      <TextInput onChangeText={(i)=>onChangeText('city',i)} placeholder style={{borderRadius:15,backgroundColor:'white',width:'50%',height:'5%'}}></TextInput>
+      <TextInput onChangeText={(i)=>onChangeText('state',i)} style={{borderRadius:15,backgroundColor:'white',width:'50%',height:'5%'}}></TextInput>
+      {/* <Button title='get weather' onPress={getWeather}></Button> */}
     </View>
   );
 }
 
+
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'black',
     alignItems: 'center',
     justifyContent: 'center',
   },
